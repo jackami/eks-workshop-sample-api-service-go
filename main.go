e73c28925f60c912684cc312697a0d63714a1e6d
jackami <tangjxl@gmail.com>
2020-09-10 10:36:55 +0800

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World 2019！"}
+		res := &response{Message: "Hello World 2019~2020！"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
